c61e4e17581667fc71238e03b8dbd2f1f5f6b75b
flip1995 <hello@philkrones.com>
2019-07-14 17:25:39 +0200

    [DNM] Test: Cache latest version of RTIM

--- setup-toolchain.sh
+++ setup-toolchain.sh
@@ -3,8 +3,15 @@
 
 cd "$(dirname "$0")"
 
+cratesio_version=$(cargo search rustup-toolchain-install-master | grep -o "[0-9]\.[0-9]\.[0-9]")
+rtim_version=$(rustup-toolchain-install-master --version | grep -o "[0-9]\.[0-9]\.[0-9]")
+
 if ! command -v rustup-toolchain-install-master > /dev/null; then
   cargo install rustup-toolchain-install-master --debug
+else
+  if [ $rtim_version != $cratesio_version ]; then
+    cargo install rustup-toolchain-install-master --debug --force
+  fi
 fi
 
 RUSTC_HASH=$(git ls-remote https://github.com/rust-lang/rust.git master | awk '{print $1}')
